e625f420b5a2d918ddfd9c006c0a1d36bbdb60e8
Priyesh Manu <priyeshmanumk@gmail.com>
2024-01-10 14:14:26 +0530

    Project change to python Django back-end. The easy way is sucessfully created.Now we want to add the diffrent ways to pick random option and a login and a signup page and the priciong page

---
+++ SmartPicker_project/home/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+import random
+# Create your views here.
+
+from django.http import HttpResponse
+
+def home(request):
+    option = []  # Initialize an empty list
+    num=0
+    choose = None
+    pick = None
+    if request.method == 'GET':
+        # Check if 'options' is in the GET parameters
+        if 'options' in request.GET:
+            num=1
+            options = request.GET['options']
+            option = options.split(",")  # Here, the entered names are turned into a list
+            length = len(option)
+            choose = random.randint(1, length - 1)
+            pick=option[choose]
+    return render(request, 'index.html', {'option': option, 'num':num, 'ch':pick,})
