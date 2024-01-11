f4910ec3f1ec9cc6bb35fe2c54973ba552fb47ee
Priyesh Manu <priyeshmanumk@gmail.com>
2024-01-11 21:00:01 +0530

    Nav bar changed and base.html created. Cut some items from the index.html and pasted it into base.html. And inherited base.html in the index.html

--- home/views.py
+++ home/views.py
@@ -16,6 +16,6 @@ def home(request):
             options = request.GET['options']
             option = options.split(",")  # Here, the entered names are turned into a list
             length = len(option)
-            choose = random.randint(1, length - 1)
+            choose = random.randint(0, length - 1)
             pick=option[choose]
     return render(request, 'index.html', {'option': option, 'num':num, 'ch':pick,})
