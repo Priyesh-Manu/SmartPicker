1533815e8ec5fcc0c754b34a464f95b626868b96
Priyesh Manu <priyeshmanumk@gmail.com>
2024-01-13 22:42:24 +0530

    An error comes when the model class register to admin. So change the models and fixed it

--- home/models.py
+++ home/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class ContactMessage:
+class ContactMessage(models.Model):
     email = models.CharField(max_length=100)
     message=models.TextField()
